538f5d204de7028dd1b1223dd543153470ab88ea
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2024-06-10 11:01:25 -0300

    js

---
+++ js/ViewPass.js
@@ -0,0 +1,9 @@
+function ViewPass() {
+    var InputPass = document.getElementById('senha')
+    if(InputPass.type === 'password'){
+        InputPass.setAttribute('type', 'text')
+    }
+    else{
+        InputPass.setAttribute('type', 'password')
+    }
+}
--- pages/signIn.php
+++ pages/signIn.php
@@ -55,7 +55,7 @@
         </div>
     </div>
 
-    <script src="../js/formsCheck.js"></script>
+    <script src="../js/ViewPass.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
